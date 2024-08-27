078d7ae3f67e85b9eb09f1306bc60ee1d8971d9c
agatha <thi.agatha21@gmail.com>
2024-08-27 16:39:46 -0300

    inicio do projeto estiloux

---
+++ teste.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int ingresso;
+    int idade;
+
+    cout << "quantos anos voce tem?" << endl;
+    cin >> idade;
+
+
+
+    cout << "quantos anos voce tem?" << endl;
+    cin >> idade;
+
+    
+    }
+
+
+
+
+
+
+
+
+
+}
